2bc7de224f07d01ed00393576cb2e2a6e9e153a5
Padyaa <zehopa@mail.ru>
2022-09-13 15:40:39 +0300

    initial commit

---
+++ src/test/java/ru/netology/FilmsManagerTest.java
@@ -0,0 +1,63 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FilmsManagerTest {
+    @Test
+    public void shouldAddFilms() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.add("First");
+        manager.add("Second");
+        manager.add("Third");
+
+        String[] expected = {"First", "Second", "Third"};
+        String[] actual = manager.findAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastFilms() {
+        FilmsManager manager = new FilmsManager(10);
+
+        manager.add("First");
+        manager.add("Second");
+        manager.add("Third");
+        manager.add("Fourth");
+        manager.add("Fifth");
+        manager.add("Sixth");
+        manager.add("Seventh");
+        manager.add("Eighth");
+        manager.add("Ninth");
+        manager.add("Tenth");
+
+        String[] expected = {"Tenth", "Ninth", "Eighth", "Seventh", "Sixth", "Fifth", "Fourth", "Third", "Second", "First"};
+        String[] actual = manager.findLast();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastSixFilms() {
+        FilmsManager manager = new FilmsManager(6);
+
+        manager.add("First");
+        manager.add("Second");
+        manager.add("Third");
+        manager.add("Fourth");
+        manager.add("Fifth");
+        manager.add("Sixth");
+        manager.add("Seventh");
+        manager.add("Eighth");
+        manager.add("Ninth");
+        manager.add("Tenth");
+
+        String[] expected = {"Tenth", "Ninth", "Eighth", "Seventh", "Sixth", "Fifth"};
+        String[] actual = manager.findLast();
+
+        assertArrayEquals(expected, actual);
+    }
+}
